6c271354fb0f25ebe96f3b89628f8bb5bb7a7277
Karina2023h <127620416+Karina2023h@users.noreply.github.com>
2023-08-07 01:19:14 +0200

    js901

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,44 @@
 import Notiflix from 'notiflix';
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  let delay = Number(form.delay.value);
+
+  for (let i = 1; i <= form.amount.value; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += Number(form.step.value);
   }
+}
+
+function createPromise(position, delay) {
+  const ef = { position, delay };
+
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(ef);
+      } else {
+        // Reject
+        reject(ef);
+      }
+    }, delay);
+  });
 }
