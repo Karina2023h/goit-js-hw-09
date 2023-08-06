b36de9d4cecf837f30ab4176fdee6a56fc7be6fa
Karina2023h <127620416+Karina2023h@users.noreply.github.com>
2023-08-06 18:06:30 +0200

    js9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const Change_Color = 1000;
+let timerId = null;
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+stopBtn.disabled = true;
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, Change_Color);
+}
+
+function onStop() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
